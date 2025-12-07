54310034bc5d6becc27f7f1a3a3df42efef22e96
shiromin639 <tranphuclongk100@gmail.com>
2025-12-07 12:02:00 +0700

    configure modelmapper

--- backend/src/main/java/com/example/course_view/payload/dto/LessonDTO.java
+++ backend/src/main/java/com/example/course_view/payload/dto/LessonDTO.java
@@ -10,4 +10,5 @@ import lombok.NoArgsConstructor;
 public class LessonDTO {
     private Long id;
     private String title;
+    private String videoUrl;
 }
--- backend/src/main/java/com/example/course_view/payload/request/LessonRequest.java
+++ backend/src/main/java/com/example/course_view/payload/request/LessonRequest.java
@@ -9,4 +9,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LessonRequest {
     private String title;
+    private String videoUrl;
 }
